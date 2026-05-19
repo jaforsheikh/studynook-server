046a31f74d0b3b0d50e858d9d69eb93edc964f9b
jaforsheikh <jaforsheikh258@gmail.com>
2026-05-19 11:33:42 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+/*
+MIDDLEWARE
+*/
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+/*
+ROUTES
+*/
+
+app.get("/", (req, res) => {
+  res.send("StudyNook Server Running");
+});
+
+/*
+SERVER
+*/
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
