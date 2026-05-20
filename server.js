936b811259c863638f5e5012eab2aee6165511e5
jaforsheikh <jaforsheikh258@gmail.com>
2026-05-20 20:32:19 +0600

    server.js page fixed

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { toNodeHandler } from "better-auth/node";
-import bookingsRoutes from "./routes/bookings.routes.js";
 
 import client from "./services/mongodb.js";
 import { auth } from "./auth.js";
 
 import roomRoutes from "./routes/roomRoutes.js";
-import bookingRoutes from "./routes/bookingRoutes.js";
+import bookingRoutes from "./routes/bookings.routes.js";
 
 dotenv.config();
 
@@ -18,11 +17,14 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
 
+/*
+Better Auth route must be before express.json()
+*/
 app.use("/api/auth", toNodeHandler(auth));
 
 app.use(express.json());
@@ -33,10 +35,14 @@ const database = client.db("studynookDB");
 export const roomsCollection = database.collection("rooms");
 export const bookingsCollection = database.collection("bookings");
 
+/*
+DATABASE CONNECTION
+*/
 async function connectDB() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
+
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.log(error);
@@ -45,6 +51,9 @@ async function connectDB() {
 
 connectDB();
 
+/*
+ROUTES
+*/
 app.use("/api/rooms", roomRoutes);
 app.use("/api/bookings", bookingRoutes);
 
@@ -52,6 +61,9 @@ app.get("/", (req, res) => {
   res.send("StudyNook Server Running");
 });
 
+/*
+SERVER
+*/
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
