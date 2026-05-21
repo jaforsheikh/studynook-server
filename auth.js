6d99561408fbde29cc2acd8f2215ae8e2e8761ac
jaforsheikh <jaforsheikh258@gmail.com>
2026-05-22 02:50:55 +0600

    auth fixed

--- auth.js
+++ auth.js
@@ -6,6 +6,7 @@ export const auth = betterAuth({
   database: mongodbAdapter(db),
 
   secret: process.env.BETTER_AUTH_SECRET,
+
   baseURL:
     process.env.BETTER_AUTH_URL ||
     "https://studynook-server-beta.vercel.app",
@@ -13,8 +14,7 @@ export const auth = betterAuth({
   trustedOrigins: [
     "http://localhost:3000",
     "https://studynook-eight.vercel.app",
-    process.env.FRONTEND_URL,
-  ].filter(Boolean),
+  ],
 
   emailAndPassword: {
     enabled: true,
@@ -29,7 +29,6 @@ export const auth = betterAuth({
   },
 
   advanced: {
-    useSecureCookies: true,
     defaultCookieAttributes: {
       sameSite: "none",
       secure: true,
