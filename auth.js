5bf976b455d7f85c7030e1c33dcdcc0a2c03160a
jaforsheikh <jaforsheikh258@gmail.com>
2026-05-21 12:53:05 +0600

    fix better auth production callback

--- auth.js
+++ auth.js
@@ -3,20 +3,17 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 import { db } from "./services/mongodb.js";
 
-const trustedOrigins = [
-  "http://localhost:3000",
-  "https://studynook-eight.vercel.app",
-  process.env.FRONTEND_URL,
-].filter(Boolean);
-
 export const auth = betterAuth({
   database: mongodbAdapter(db),
 
   secret: process.env.BETTER_AUTH_SECRET,
 
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: "https://studynook-server-2.onrender.com",
 
-  trustedOrigins,
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://studynook-eight.vercel.app",
+  ],
 
   emailAndPassword: {
     enabled: true,
